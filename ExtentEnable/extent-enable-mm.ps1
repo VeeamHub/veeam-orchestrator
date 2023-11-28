f06ccd6cc3bbc49aa1f945186f43890f06d1d35e
skitch210 <marty.williams@veeam.com>
2023-11-28 14:56:04 -0500

    add loop for multiple extents

--- ExtentEnable/extent-enable-mm.ps1
+++ ExtentEnable/extent-enable-mm.ps1
@@ -1,24 +1,41 @@
 #Please Update the vbr-info.csv for your settings
 $vbrCSV = "C:\VRO\Scripts\VBR\vbr-info.csv" #CSV File to read from.
-$vbrInfo =Import-Csv $vbrCSV
+$vbrInfo =Import-Csv $vbrCSV 
 
+# Connect to VBR server defined in csv file
 $connVBR = Connect-VBRServer -Server $vbrInfo.vbrserver
-Write-Output "SOBR selected: " $vbrInfo.repo
+Write-Host "SOBR selected: $($vbrInfo.repo)"
 
+# Get the SOBR defined in csv file
 $scaleoutrepository = Get-VBRBackupRepository -Scaleout -Name $vbrInfo.repo
 
-Write-Output "Capacity tier selected: " $scaleoutrepository.CapacityExtent.Repository.Name
+Write-Host "Capacity tier selected: $($scaleoutrepository.CapacityExtent.Repository.Name)"
 
+# Get the Performance Extents from the SOBR
 $extent = Get-VBRRepositoryExtent -Repository $scaleoutrepository
 
-Write-Output "Extent Status: " $extent[0].Status
+Write-Host "Number of Perfomance Extents: $($extent.Count)"
 
-$extentMM = Enable-VBRRepositoryExtentMaintenanceMode -Extent $extent[0]
+# Loop through the extents and write back current status
+for ($i=0; $i -lt $extent.Length; $i++)
+{
+   Write-Host "Extent: $($extent.Name) is $($extent[$i].Status)"
+}
 
-Write-Output "Enable maintenance mode: " $extentMM.Result
+# Loop through Extents and place into Maintenance Mode
+for ($a=0; $a -lt $extent.Length; $a++)
+{
+   $extentMM = Enable-VBRRepositoryExtentMaintenanceMode -Extent $extent[$a]
+}
 
-$extent = Get-VBRRepositoryExtent -Repository $scaleoutrepository
+# Get status of Extents
+$extentSts = Get-VBRRepositoryExtent -Repository $scaleoutrepository
 
-Write-Output "Extent Status: " $extent[0].Status
+# Loop through Extents and write back current status
+for ($b=0; $b -lt $extent.Length; $b++)
+{
+   Write-Host "Extent: $($extent.Name) is $($extentSts[$b].Status)"
+}
 
+# Disconnect from Backup server
 Disconnect-VBRServer
